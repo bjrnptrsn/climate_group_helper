2ccbeabb28981347867949203fc02b038efb81ed
Richard Caller <rcaller@email.com>
2025-11-26 21:34:42 +0000

    Turn off devices that dont support temperature

--- custom_components/climate_group_helper/climate.py
+++ custom_components/climate_group_helper/climate.py
@@ -922,6 +922,20 @@ class ClimateGroup(GroupEntity, ClimateEntity):
             else:
                 _LOGGER.debug("No entities support the target temperature feature, skipping service call")
 
+        if (entity_ids := self._get_unsupporting_entities(ATTR_SUPPORTED_FEATURES, ClimateEntityFeature.TARGET_TEMPERATURE)):
+                data = {
+                    ATTR_ENTITY_ID: entity_ids,
+                    ATTR_HVAC_MODE: 'off'
+                }
+
+                _LOGGER.debug("Setting temperature: %s", data)
+                await self.hass.services.async_call(
+                    CLIMATE_DOMAIN, SERVICE_SET_HVAC_MODE, data, blocking=True, context=context or self._context
+                )
+                executed = True
+            else:
+                _LOGGER.debug("No entities support the target temperature feature, skipping service call")
+
         if ATTR_TARGET_TEMP_LOW in kwargs and ATTR_TARGET_TEMP_HIGH in kwargs:
             if (entity_ids := self._get_supporting_entities(ATTR_SUPPORTED_FEATURES, ClimateEntityFeature.TARGET_TEMPERATURE_RANGE)):
                 data = {
